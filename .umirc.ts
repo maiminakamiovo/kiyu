6acd225596e63a974c6982c87e362743151ad934
wuqiying <qiying_wu@ohsa.com.cn>
2022-07-25 10:43:43 +0800

    wqy

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@umijs/max";
+import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
   antd: {},
@@ -7,40 +7,40 @@ export default defineConfig({
   initialState: {},
   request: {},
   layout: {
-    title: "umi",
+    title: 'umi',
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
   },
 
   routes: [
     {
-      path: "/",
-      redirect: "/home",
+      path: '/',
+      redirect: '/home',
     },
 
     {
-      name: "首页",
-      path: "/home",
-      component: "./Home",
+      name: '首页',
+      path: '/home',
+      component: './Home',
     },
 
     {
-      name: "增删改查示例",
-      path: "/access",
-      component: "./Access",
+      name: '增删改查示例',
+      path: '/access',
+      component: './Access',
     },
 
     {
-      name: "Table示例",
-      path: "/table",
-      component: "./Table",
+      name: 'Table示例',
+      path: '/table',
+      component: './Table',
     },
     {
-      name: " flex示例",
-      path: "/hookls",
-      component: "./Hookls",
+      name: ' flex示例',
+      path: '/hookls',
+      component: './Hookls',
     },
   ],
 
-  npmClient: "npm",
+  npmClient: 'npm',
   dva: {},
 });
