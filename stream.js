c599a73fa0ea95e4285c880350ca6e33f7357071
wuqiying <qiying_wu@ohsa.com.cn>
2022-08-24 17:50:07 +0800

    wqy

--- stream.js
+++ stream.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const data = '';
+let data = '';
 
 // 创建可读流
 const readerStream = fs.createReadStream('index.txt');
@@ -7,12 +7,29 @@ const readerStream = fs.createReadStream('index.txt');
 readerStream.setEncoding('UTF8');
 // 处理事件流 --> data end and error
 readerStream.on('data', function (chunk) {
-  // data += chunk
+  data += chunk;
 });
 readerStream.on('end', function () {
   console.log(data);
 });
 readerStream.on('error', function () {
+  console.log(err.stack);
+});
+
+console.log('程序执行完毕');
+
+// 文件写入流
+const writerStream = fs.createWriteStream('index.txt');
+// 使用UTF8写入数据
+let data2 = '双城记';
+writerStream.write(data2, 'UTF8');
+//标记文件末尾
+writerStream.end();
+// 处理流事件 --> finish,error
+writerStream.on('finish', function () {
+  console.log('写入完成');
+});
+writerStream.on('error', function (err) {
   console.log(err.stack);
 });
 
